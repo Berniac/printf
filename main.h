be9cb4ef3e4e64ee851c0d9d0fb2c508738cf92b
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-04-18 13:22:01 -0700

    header

--- main.h
+++ main.h
@@ -5,10 +5,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+char *t;
+int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+int print_int(va_list arguments, char *buf, unsigned int ibuf);
+int print_reverse(char *s);
 
-#endif
+#endif  /* _MAIN_H */
