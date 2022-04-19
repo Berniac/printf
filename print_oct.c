557e62d9d465b7ae6fbefc8320d47d68e4144469
BernieW <87444402+Berniac@users.noreply.github.com>
2022-04-19 10:22:15 +0300

    Update print_oct.c

--- print_oct.c
+++ print_oct.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * print_oct - prints decimal number in octal
- * @arguments: input number
+ * @ptr: input number
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
+ *
  * Return: number of chars printed.
  */
 int print_oct(va_list ptr, char *buf, unsigned int ibuf)
